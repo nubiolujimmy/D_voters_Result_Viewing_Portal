8f14506a682050ecf2fd7050bb74452b8877d296
olujimmy <nubiolujimmy@gmail.com>
2023-03-22 17:52:47 -0500

    first commit

---
+++ contracts/Dvot.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.2;
+contract Dvot {
+  // string public dat_N = "Dvot";
+
+
+   constructor(){
+
+   }
+
+
+
+
+
+
+
+}
---
+++ migrations/1679498775_deploy_contracts.js
@@ -0,0 +1,6 @@
+const Dvot = artifacts.require("Dvot");
+
+module.exports = function(deployer) {
+  deployer.deploy(Dvot);
+};
+
